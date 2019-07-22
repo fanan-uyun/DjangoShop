34e9d019b49fd8f9456a4684c4046aa7134fa530
aa <yunfancom@sina.com>
2019-07-22 15:05:13 +0800

    截图上传

---
+++ Store/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from Store.views import *
+
+urlpatterns = [
+    path('register/', register),
+]
--- Store/views.py
+++ Store/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    return render(request,"store/register.html")
